46157b318b8ef681c93ee5b493e78c98af2dec91
Paco Gomez <fjgomez@florida-uni.es>
2014-05-04 08:06:44 +0200

    Ejercicio Ficha policia

---
+++ FichaPolicia/src/Delincuente.java
@@ -0,0 +1,19 @@
+
+public class Delincuente {
+	//Falta por crear
+	//Atributos para guardar
+	//Tienen que ser los atributos de la pantalla
+
+	public Delincuente() {
+		
+	}
+	//Falta por crear
+	//Los métodos de guardado y recuperación
+	
+	//Falta por crear
+	//El método toString para que aparezca en el comboBox
+	public String toString(){
+		return ;
+	}
+
+}
