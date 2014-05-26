036a91b195b92fab5cebbb749b18f2531b2ce0d6
pacogomezedutic <edutic.es@gmail.es>
2014-05-26 10:27:05 +0200

    Ejemplo con ventana y JComboBox

--- EjercicioDB/src/BD.java
+++ EjercicioDB/src/BD.java
@@ -3,30 +3,26 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.JComboBox;
+
 import com.mysql.jdbc.Statement;
 
 
-public class EjercicioDB {
+public class BD {
+	Connection conexion = null; //maneja la conexión
+	Statement instruccion = null;// instrucción de consulta
+	ResultSet conjuntoResultados = null;// maneja los resultados
+	JComboBox<Liga> liga;
+	
+	public BD(JComboBox liga) {
 
-	public static void main(String[] args) {
-		Connection conexion = null; //maneja la conexión
-		Statement instruccion = null;// instrucción de consulta
-		ResultSet conjuntoResultados = null;// maneja los resultados
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			// establece la conexión a la base de datos
 			conexion = DriverManager.getConnection("jdbc:mysql://localhost/apuestas","root","tonphp");
 			// crea objeto Statement para consultar la base de datos
 			instruccion = (Statement) conexion.createStatement();
-			// consulta la base de datos
-			conjuntoResultados = instruccion.executeQuery("SELECT idLiga,Nombre FROM liga");
-			//Mostrar por pantalla
-			while (conjuntoResultados.next())
-			{
-			   System.out.println("id="+conjuntoResultados.getObject("idLiga")+
-			      ", Nombre="+conjuntoResultados.getObject("Nombre"));
-			}
-			conjuntoResultados.close();
+			this.liga=liga;
 		}catch( SQLException excepcionSql ){
 			excepcionSql.printStackTrace();
 		}// fin de catch
@@ -34,6 +30,26 @@ public class EjercicioDB {
 		{
 			noEncontroClase.printStackTrace();
 		}// fin de catch
+	}
+	
+	public void leerLigas(){
+		try{
+			instruccion = (Statement) conexion.createStatement();
+			// consulta la base de datos
+			conjuntoResultados = instruccion.executeQuery("SELECT idLiga,Nombre FROM liga");
+			//Mostrar por pantalla
+			while (conjuntoResultados.next())
+			{
+				Liga l=new Liga();
+				l.setNombre((String)conjuntoResultados.getObject("Nombre"));
+				liga.addItem(l);
+			   System.out.println("id="+conjuntoResultados.getObject("idLiga")+
+			      ", Nombre="+conjuntoResultados.getObject("Nombre"));
+			}
+			conjuntoResultados.close();
+		}catch( SQLException excepcionSql ){
+			excepcionSql.printStackTrace();
+		}// fin de catch	
 	}
 
 }
---
+++ EjercicioDB/src/Liga.java
@@ -0,0 +1,16 @@
+
+public class Liga {
+	private String nombre="";
+	public Liga() {
+		
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre=nombre;
+	}
+	
+	public String toString(){
+		return nombre;
+	}
+
+}
