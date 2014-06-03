0869b694bcf21e0653999aed6a5b2acb17a3755b
Paco Gomez <fjgomez@florida-uni.es>
2014-06-03 15:36:57 +0200

    JTable ejemplo finalizado
    Signed-off-by: Paco Gomez <fjgomez@florida-uni.es>

--- JTable/src/JTableExample.java
+++ JTable/src/JTableExample.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
@@ -13,11 +14,17 @@ import java.sql.SQLException;
 
 import com.mysql.jdbc.Statement;
 
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+
 
 public class JTableExample extends JFrame {
 
 	private JPanel contentPane;
 	private DefaultTableModel dtmEjemplo;
+	private JTable table;
+	private JScrollPane scpEjemplo;
 
 	/**
 	 * Launch the application.
@@ -45,21 +52,42 @@ public class JTableExample extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
 		
 		//JTABLE
-        //Llenamos el modelo
-        dtmEjemplo = new DefaultTableModel(null,rellenarColumnas());
-		
+        //1.- Definimos el modelo de JTable, en nuestro caso DefaultModel
+		//2.- Definimos los títulos de las columnas
+		//3.- REllenamos las filas a partir de la consulta a la base de datos
+		//3.- Creamos el JTable y le asignamosel modelo rellenado
+		//4,. Ponemos el JTable dentro de un JScrollPane para poder hacer scroll
+		//5.- Damos visivilidad al JTable con setViewportView
+        dtmEjemplo = new DefaultTableModel(null,rellenarTitColumnas());
+        
 		//CONEXION A BASE DE DATOS
-		conexionDB();
+		conexionDB();       
+		
+        table = new JTable();
+		table.setModel(dtmEjemplo);
+	
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 10, 414, 241);
+		scrollPane.add(table);
+		//REvisar la documentación para ver que significa el JScrollPane
+		//http://docs.oracle.com/javase/7/docs/api/javax/swing/JScrollPane.html
+		scrollPane.setViewportView(table);
+		contentPane.add(scrollPane);
+		
+
 	}
 	
     //Encabezados de la tabla
-    private String[] rellenarColumnas()
+    private String[] rellenarTitColumnas()
     {
-          String columna[]=new String[]{"idLiga","Nombre"};
+          String columna[]=new String[]{"idLiga","nombre","numEquipos"};
           return columna;
     }
 	
@@ -70,17 +98,19 @@ public class JTableExample extends JFrame {
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			// establece la conexión a la base de datos
-			conexion = DriverManager.getConnection("jdbc:mysql://localhost/apuestas","root","tonphp");
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost/apuestas","root","");
 			// crea objeto Statement para consultar la base de datos
 			instruccion = (Statement) conexion.createStatement();
 			// consulta la base de datos
-			conjuntoResultados = instruccion.executeQuery("SELECT idLiga,Nombre FROM liga");
-			//Mostrar por pantalla
-			while (conjuntoResultados.next())
-			{
-			   System.out.println("id="+conjuntoResultados.getObject("idLiga")+
-			      ", Nombre="+conjuntoResultados.getObject("Nombre"));
-			}
+			conjuntoResultados = instruccion.executeQuery("SELECT idLiga,nombre,numEquipos FROM ligas");
+			//Añadir datos al modelo
+	        Object datos[]=new Object[3]; //Numero de columnas de la tabla
+	        while (conjuntoResultados.next()) {
+	        	for (int i = 0; i < 3; i++) {
+	            	datos[i] = conjuntoResultados.getObject(i + 1);
+	            }
+	        	dtmEjemplo.addRow(datos);
+	        }
 			conjuntoResultados.close();
 		}catch( SQLException excepcionSql ){
 			excepcionSql.printStackTrace();
@@ -90,5 +120,4 @@ public class JTableExample extends JFrame {
 			noEncontroClase.printStackTrace();
 		}// fin de catch		
 	}
-
 }
